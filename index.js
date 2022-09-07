3c4bf335e9db10dff55fd1aa24a607f8f739fcae
Rhea Dieberger <rdieberger@gmail.com>
2022-09-07 17:56:17 +0200

    get urls

---
+++ index.js
@@ -0,0 +1,18 @@
+import fs from 'node:fs/promises';
+import { load } from 'cheerio';
+import Blob from 'fetch-blob';
+import fetch from 'node-fetch';
+
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+
+const body = await response.text();
+
+const $ = load(body);
+
+const imgSrcs = [];
+
+for (let i = 0; i < 10; i++) {
+  imgSrcs.push($('section > div > a > img')[i].attribs.src);
+}
